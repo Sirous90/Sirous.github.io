0fa62e424699abd4568e5c07225993159b54a448
Sirous90 <61553081+Sirous90@users.noreply.github.com>
2020-10-18 08:17:10 -0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-  function myFunction() {
-  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");}  
-  if (confirm("Voulez vous aller sur xxxvidsxxx ? ")) {
-   myFunction();
-  }
-  else {
-    
-  }	
